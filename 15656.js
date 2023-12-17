62d1113c48a6a2ba628f4a9a6aa7c0aa2f382e28
GrowFB <sybt1017@naver.com>
2023-12-18 01:03:01 +0900

    백준 문제풀이

---
+++ 15656.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt").toString().trim().split("\n");
+
+let Numb = input[0].split(" ");
+let N = parseInt(Numb[0]);
+let M = parseInt(Numb[1]);
+let num = input[1].split(" ").map(Number);
+
+num.sort((a, b) => a - b);
+
+let result = [];
+let arr = Array.from({ length: M }, () => 0);
+let isUsedNum = Array.from({ length: N + 1 }, () => false);
+dfs(0);
+
+function dfs(n) {
+  if (n === M) {
+    result.push(arr.join(" "));
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    arr[n] = num[i];
+
+    dfs(n + 1);
+  }
+}
+console.log(result.join("\n"));
