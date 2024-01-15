9e87d40c25d12c060c80fc4e6bbf2c7324caa946
GrowFB <sybt1017@naver.com>
2024-01-16 00:23:57 +0900

    백준문제풀이

--- 4949.js
+++ 4949.js
@@ -1,46 +1,40 @@
-const fs = require('fs'); 
+const fs = require("fs");
 const input = fs.readFileSync("input.txt").toString().trim().split("\n");
 
-let cnt = 0 ;
+let cnt = 0;
 // let cnt2 = 0 ;
 let temp = [];
 
-for(let j = 0 ; j < input.length ; j++){
-  for(let i = 0 ; i < input[j].length ; i++){
-  
-  if(input[j].charAt(i) == '('){
-    temp.push('(')
-  }
-  if(input[j].charAt(i) == ')'){
-   if(temp[temp.length-1] == '('){
-    temp.pop('')
-   }else {
-    temp.push(')')
-   }
-
-  }
-  if(input[j].charAt(i) == '['){
-   temp.push('[')
-  }
-  if(input[j].charAt(i) == ']'){
-    if(temp[temp.length-1] == '['){
-     temp.pop('')
-    }else {
-     temp.push(']')
+for (let j = 0; j < input.length; j++) {
+  for (let i = 0; i < input[j].length; i++) {
+    if (input[j].charAt(i) == "(") {
+      temp.push("(");
     }
- 
-   }
-
-}
-
-if(input[j].charAt(0) != '.'){
-  if(temp.length == 0 ){
-    console.log('yes')
-  }else {
-    console.log('no')
+    if (input[j].charAt(i) == ")") {
+      if (temp[temp.length - 1] == "(") {
+        temp.pop("");
+      } else {
+        temp.push(")");
+      }
+    }
+    if (input[j].charAt(i) == "[") {
+      temp.push("[");
+    }
+    if (input[j].charAt(i) == "]") {
+      if (temp[temp.length - 1] == "[") {
+        temp.pop("");
+      } else {
+        temp.push("]");
+      }
+    }
   }
-}
- temp = [];
 
-
+  if (input[j].charAt(0) != ".") {
+    if (temp.length == 0) {
+      console.log("yes");
+    } else {
+      console.log("no");
+    }
+  }
+  temp = [];
 }
