7330096662c3e7bb27b81800b9f41a7d6d0c337c
GrowFB <sybt1017@naver.com>
2024-02-21 00:21:08 +0900

    백준문제풀이

--- 4101.js
+++ 4101.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt").toString().trim().split("\n");
 
-let a = input[0].split(" ").map(Number);
-
 for (let i = 0; i < input.length; i++) {
   let a = input[i].split(" ").map(Number);
   if (a[0] > a[1]) {
